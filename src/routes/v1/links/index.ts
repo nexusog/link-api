b28277c453808447a46115db460e39d2f1a78a6b
Henil Malaviya <henilm.dev@gmail.com>
2024-12-12 00:54:55 +0530

    feat: GET /links/{id} route

--- src/routes/v1/links/index.ts
+++ src/routes/v1/links/index.ts
@@ -1,9 +1,11 @@
 import { baseElysia } from '@/base'
 import { LinkCreateRoute } from './create'
 import { LinkRedirectRoute } from './redirect'
+import { LinkGetRoute } from './get'
 
 export const LinksRoutes = baseElysia({
 	prefix: '/links',
 })
 	.use(LinkCreateRoute)
+	.use(LinkGetRoute)
 	.use(LinkRedirectRoute)
