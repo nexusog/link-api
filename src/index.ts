c51a54790c389d3a8b0d80f3d09cce2dc7a33699
Henil Malaviya <henilm.dev@gmail.com>
2024-12-21 16:07:56 +0530

    feat: add IP Hash logging

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { baseElysia } from '@/base'
 import { logger as NiceLogger } from '@tqman/nice-logger'
 import { logger } from '@/utils/logger'
 import { V1Routes } from '@/routes/v1'
-import { t } from 'elysia'
+import Elysia, { t } from 'elysia'
 import cors from '@elysiajs/cors'
 import { compression } from 'elysia-compression'
 import { env } from '@/lib/env'
@@ -30,6 +30,16 @@ export const app = baseElysia({
 				return new Date().toISOString()
 			},
 		}),
+	)
+	.use(
+		new Elysia({
+			name: 'IpHashLogger',
+		})
+			.use(ip())
+			.resolve(({ ip }) => {
+				logger.info(`IP: ${Bun.hash(JSON.stringify(ip)).toString()}`)
+			})
+			.as('plugin'),
 	)
 	.use(
 		swagger({
